fd155a87f84b650f21f51723a763dbff176dc7d2
Keeran Bezuidenhout <keeranbezuidenhout@gmail.com>
2020-08-26 16:54:59 +0200

    Sequential basin finding.

--- src/TerrainClassification.java
+++ src/TerrainClassification.java
@@ -1,5 +1,6 @@
-import java.io.File; 
+import java.io.*; 
 import java.util.Scanner; 
+import java.util.ArrayList;
 
 /** Driver class for the Terrain Classification application.
  */
@@ -7,6 +8,8 @@ public class TerrainClassification {
     //#region Statif variables
 
     private static Terrain grid;
+    private static ArrayList<GridCell> outputCells = new ArrayList<GridCell>();
+    
 
     //#endregion
    
@@ -18,7 +21,7 @@ public class TerrainClassification {
     * @param filename Input file name of terrain data. 
     * @return True when terrain generation is complete.
     */
-   public static boolean constructTerrain(String filepath) {
+   public static void constructTerrain(String filepath) {
         try {
             // read in the file and create terrain object
             File inputFile = new File(filepath);
@@ -32,22 +35,121 @@ public class TerrainClassification {
                 for(int c = 0; c < grid.getColumns(); c++) {
                     double height = Double.parseDouble(sc.next());
                     grid.setCell(new GridCell(height, r, c), r, c);
+                             
+
                 }
             }
             
-            System.out.println("Loading complete.");
             sc.close();
-
-            return true;
+            System.out.println("Loading complete.");
 
         } catch (Exception e) {
             System.out.println(e);
-
-
-            return false;
         }          
    }
 
+
+   /** Finding the basin cells sequentially.
+    * 
+    * @return
+    */
+   public static void findBasinSequential() {
+    // adding heights of neighbouring cells to non-boundary cells.
+    for (int r = 1; r < grid.getRows() - 1; r++) {
+        for(int c = 1; c < grid.getColumns() - 1; c++) {
+            double[] peerHeights = new double[8];
+            peerHeights[0] = grid.getCell(r-1, c-1).getHeight();
+            peerHeights[1] = grid.getCell(r-1, c).getHeight();
+            peerHeights[2] = grid.getCell(r-1, c+1).getHeight();
+            peerHeights[3] = grid.getCell(r, c-1).getHeight();
+            peerHeights[4] = grid.getCell(r, c+1).getHeight();
+            peerHeights[5] = grid.getCell(r+1, c-1).getHeight();
+            peerHeights[6] = grid.getCell(r+1, c).getHeight();
+            peerHeights[7] = grid.getCell(r+1, c+1).getHeight();
+
+            grid.getCell(r, c).setBasin(true);
+            for (double pHeight : peerHeights){
+                if ( grid.getCell(r, c).getHeight() > pHeight ) {
+                    grid.getCell(r, c).setBasin(false);
+                }
+            }
+            outputCells.add(grid.getCell(r, c));
+        }
+    }
+   }
+   
+   /**
+    * Printing out basins and peaks.
+    */
+   public static void printOutput() {
+       int basins = 0;
+       for (GridCell cell: outputCells) {
+           if (cell.isBasin()) {basins++;}
+       }
+
+       System.out.println(basins);
+
+       for (GridCell cell: outputCells) {
+        if (cell.isBasin()) {
+            System.out.println(cell.getRow() + " " + cell.getColumn() );
+        }
+    }
+   }
+
+   /**
+    * Printing out basins and peaks to file.
+    */
+    public static void printOutput(String filename) {
+        int basins = 0;
+        for (GridCell cell: outputCells) {
+            if (cell.isBasin()) {basins++;}
+        }
+        
+        try {
+            File outputFile = new File(filename);
+	        FileOutputStream fos = new FileOutputStream(outputFile);
+ 	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            
+            bw.write(Integer.toString(basins));
+            bw.newLine();
+            
+            for (GridCell cell: outputCells) {
+                if (cell.isBasin()) {
+                    bw.write(cell.getRow() + " " + cell.getColumn());
+                    bw.newLine();
+                }
+            }
+            
+            bw.close();
+
+            System.out.println("File " + filename + " has been created successfully.");
+
+
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+   
+   
+   /**  Prints grid of cell with surrounding neighbours.
+    *   
+    * @param r Row of cell.
+    * @param c Column of cell.
+    */
+   public static void printCellGrid(int r, int c) {
+    System.out.print(grid.getCell(r-1, c-1).getHeight() + "\t\t");
+    System.out.print(grid.getCell(r-1, c).getHeight() + "\t\t");
+    System.out.println(grid.getCell(r-1, c+1).getHeight());
+    
+    System.out.print(grid.getCell(r, c-1).getHeight() + "\t\t");
+    System.out.print(grid.getCell(r, c).getHeight() + "\t\t");
+    System.out.println(grid.getCell(r, c+1).getHeight());
+    
+    System.out.print(grid.getCell(r+1, c-1).getHeight() + "\t\t");
+    System.out.print(grid.getCell(r+1, c).getHeight() + "\t\t");
+    System.out.println(grid.getCell(r+1, c+1).getHeight());
+   }
    //#endregion
    
 
@@ -59,10 +161,10 @@ public class TerrainClassification {
      */
     public static void main (String[] args) {
         String filepath = "data/small_in.txt";
-        boolean gridGenerated = false;
 
-        gridGenerated = constructTerrain(filepath); // should be selected by GUI
-        System.out.println(gridGenerated);
+        constructTerrain(filepath); // should be selected by GUI
+        findBasinSequential();
+        printOutput("data/test_out_small.txt");
     }
 
     //#endregion
